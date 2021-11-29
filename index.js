bacc9fc2df337e550e0cec74c64d44d200e28192
Travis Wise <86330962+TravisMWise@users.noreply.github.com>
2021-11-29 01:36:20 -0500

    Setup file structure and connected to mongoDB

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://Travis:Travis123@interactive-diary.4eoju.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message)
+);
